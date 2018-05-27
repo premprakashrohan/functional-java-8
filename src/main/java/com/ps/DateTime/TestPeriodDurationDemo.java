f4170ed90e4414b40219eb096ec3f7b6a581a2ab
ishi <ishi@ishi-desktop>
2018-05-27 23:57:32 +0530

    Date Time added

---
+++ src/main/java/com/ps/DateTime/TestPeriodDurationDemo.java
@@ -0,0 +1,41 @@
+package com.ps.DateTime;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
+
+public class TestPeriodDurationDemo {
+
+	public static void main(String[] args) {
+		TestPeriodDurationDemo demo = new TestPeriodDurationDemo();
+		demo.testPeriod();
+		demo.testDuration();
+
+	}
+	
+	 public void testPeriod() {
+	      //Get the current date
+	      LocalDate date1 = LocalDate.now();
+	      System.out.println("Current date: " + date1);
+			
+	      //add 1 month to the current date
+	      LocalDate date2 = date1.plus(1, ChronoUnit.MONTHS);
+	      System.out.println("Next month: " + date2);
+	      
+	      Period period = Period.between(date2, date1);
+	      System.out.println("Period: " + period);
+	   }
+		
+	   public void testDuration() {
+	      LocalTime time1 = LocalTime.now();
+	      Duration twoHours = Duration.ofHours(2);
+			
+	      LocalTime time2 = time1.plus(twoHours);
+	      Duration duration = Duration.between(time1, time2);
+			
+	      System.out.println("Duration: " + duration);
+	   }
+
+}
---
+++ src/main/java/com/ps/DateTime/package-info.java
@@ -0,0 +1,21 @@
+/**
+ * With Java 8, a new Date-Time API is introduced to cover the following drawbacks of old date-time API.
+
+    Not thread safe − java.util.Date is not thread safe, thus developers have to deal with concurrency issue while using date. The new date-time API is immutable and does not have setter methods.
+
+    Poor design − Default Date starts from 1900, month starts from 1, and day starts from 0, so no uniformity. The old API had less direct methods for date operations. The new API provides numerous utility methods for such operations.
+
+    Difficult time zone handling − Developers had to write a lot of code to deal with timezone issues. The new API has been developed keeping domain-specific design in mind.
+
+Java 8 introduces a new date-time API under the package java.time. Following are some of the important classes introduced in java.time package.
+
+    Local − Simplified date-time API with no complexity of timezone handling.
+
+    Zoned − Specialized date-time API to deal with various timezones.
+
+ */
+/**
+ * @author ishi
+ *
+ */
+package com.ps.DateTime;
